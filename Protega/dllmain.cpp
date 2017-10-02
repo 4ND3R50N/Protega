c6f729cd2694021d0652b9a60bf38696a2c7fde4
4ND3R50N <l.pickelin@web.de>
2017-10-02 16:00:18 +0200

    Formatted code

--- Protega/dllmain.cpp
+++ Protega/dllmain.cpp
@@ -1,10 +1,8 @@
-// dllmain.cpp : Definiert den Einstiegspunkt für die DLL-Anwendung.
 #include "stdafx.h"
 
 extern "C" int __declspec(dllexport) __cdecl PEntryMain()
 {
+		
 	MessageBoxA(NULL, "Protega is running!", "Protega antihack engine", NULL);
 	return 1;
 }
-
-
