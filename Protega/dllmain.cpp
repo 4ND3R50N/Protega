1ef6453a2f7e16137b12c7eae1119609f4d8298d
4ND3R50N <l.pickelin@web.de>
2017-10-03 15:44:07 +0200

    Formatted code

--- Protega/dllmain.cpp
+++ Protega/dllmain.cpp
@@ -1,8 +1,7 @@
 #include "stdafx.h"
 
 extern "C" int __declspec(dllexport) __cdecl PEntryMain()
-{
-		
+{		
 	MessageBoxA(NULL, "Protega is running!", "Protega antihack engine", NULL);
 	return 1;
 }
