bb68ba6cff053d9ca582b82e64338bb7dad1af48
4ND3R50N <l.pickelin@web.de>
2017-10-04 19:12:11 +0200

    Threaded call of the ProtegaCore entry point

--- Protega/dllmain.cpp
+++ Protega/dllmain.cpp
@@ -1,7 +1,33 @@
 #include "stdafx.h"
+#include "Core/ProtegaCore.h"
 
-extern "C" int __declspec(dllexport) __cdecl PEntryMain()
-{		
-	MessageBoxA(NULL, "Protega is running!", "Protega antihack engine", NULL);
-	return 1;
+bool bStopper = false;
+void initAntihack();
+
+extern "C"
+{
+	int __declspec(dllexport) __cdecl PEntryMain()
+	{
+		
+		CreateThread(NULL, NULL, LPTHREAD_START_ROUTINE(initAntihack), NULL, 0, 0);
+		return 1;
+	}
+
+	//Just for testing reasons
+	int __declspec(dllexport) stopTrigger()
+	{
+		while (bStopper == false)
+		{
+			if (bStopper == true)
+			{
+				break;
+			}
+		}
+		return true;
+	}
+}
+void initAntihack()
+{
+	ProtegaCore *Antihack = new ProtegaCore();
+	Antihack->StartAntihack();
 }
