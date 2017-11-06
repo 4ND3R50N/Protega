a3582a3caa6fe3df8bbf3f6f355897a5585a4b08
Ferity145 <32592869+Ferity145@users.noreply.github.com>
2017-11-06 22:20:49 +0100

    Server - Commit Test

--- Protega - Server/Program.cs
+++ Protega - Server/Program.cs
@@ -12,6 +12,7 @@ namespace Protega___Server
     {
         static void Main(string[] args)
         {
+            string Test = "123";
             ControllerCore Controller = new ControllerCore(1, ';', 'a', "asdf", "mssql", "217.23.14.23", 1433, "sa", "S3mad0123", "Network", String.Format(@"{0}/Test.txt", Directory.GetCurrentDirectory()));
             Controller.Start();
             Console.ReadLine();
