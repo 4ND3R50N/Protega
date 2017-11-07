bdb6e355894d89781e65b41c99c5fa5e2db2a99c
4ND3R50N <l.pickelin@web.de>
2017-11-07 14:44:25 +0100

    Added first download logic parts

--- Protega/Core/Data_Manager.cpp
+++ Protega/Core/Data_Manager.cpp
@@ -1,3 +1,23 @@
 #include "../stdafx.h"
 #include "Data_Manager.h"
 
+
+void WriteMemoryCallback(void *ptr, size_t size, size_t nmemb, void *stream) {
+	const char* test = (const char*)ptr;
+}
+
+
+std::string Data_Manager::GetWebFileAsString()
+{
+	CURL *curl;
+	CURLcode res;
+	curl = curl_easy_init();
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, "http://62.138.6.50:13011/CabalOnline/Heuristic_Data.csv");
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+		curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
+	}
+
+	return std::string();
+}
--- Protega/Core/Data_Manager.h
+++ Protega/Core/Data_Manager.h
@@ -1,9 +1,13 @@
 #pragma once
+#include <iostream>
+#include <curl.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 class Data_Manager
 {
 private:
-
+	std::string GetWebFileAsString();
 public:
 	
 };
