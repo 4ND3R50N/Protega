4bbb541773c571eb5ef675e330f9cb548e929506
4ND3R50N <l.pickelin@web.de>
2017-11-14 07:53:28 +0100

    Added file conversion routine + DynamicDataCollecting test

--- Protega - Client Testsuite/GlobalCoreFunctionTests.cpp
+++ Protega - Client Testsuite/GlobalCoreFunctionTests.cpp
@@ -24,6 +24,9 @@ namespace ProtegaClientTestsuite
 		//AES
 		const std::string sMessage = "Katze";
 
+		//Data Gathering
+		const std::string sHttpRequest = "Test!";
+
 	public:
 	    //Triggers the main entry point of protega.dll -> maybe also get return
 		TEST_METHOD(Core_CompleteStartUpProtega)
@@ -56,14 +59,48 @@ namespace ProtegaClientTestsuite
 
 		}
 
-		TEST_METHOD(Core_DataGathering)
-		{
-		
-			const char* test = Data_Manager::GetTargetEnviormentDataUrl();
-			
-			
-			Data_Manager::GetWebFileAsString("http://62.138.6.50:13011/CabalOnline/Heuristic_Data.csv");
+		TEST_METHOD(Core_WebDataGathering)
+		{			
+			Assert::AreEqual(Data_Manager::GetWebFileAsString("http://62.138.6.50:13011/index.html"), sHttpRequest);
 		}
+		TEST_METHOD(Core_DynamicDataCollecting)
+		{
+			Data_Manager::CollectDynamicProtesData();
+		}
+
+		TEST_METHOD(Task_ConvertFileToEnc)
+		{
+			std::ifstream infile;
+			std::string sTmp;
+			std::string input;
+			std::string Dateidecryption;
+			infile.open("VMP_Addresses.csv");
+			while (getline(infile, sTmp))
+			{
+				input.append(sTmp.c_str());
+			}
+			infile.close();
+			sTmp.clear();
+
+			std::string test = CryptoPP_AES_Converter::Encrypt("1234567890123456", "bbbbbbbbbbbbbbbb", input);
+			//Safe encode to file
+			std::ofstream file("VMP_Addresses.csv.enc");
+			file << test;
+			file.close();
+			input.clear();
+
+			//Normal call
+			std::string DirekteDecryption = CryptoPP_AES_Converter::Decrypt("1234567890123456", "bbbbbbbbbbbbbbbb", test);
+			//Read in call
+			infile.open("VMP_Addresses.csv.enc");
+			while (getline(infile, sTmp))
+			{
+				Dateidecryption.append(sTmp.c_str());
+			}
+			infile.close();		
+			std::string test2 = CryptoPP_AES_Converter::Decrypt("1234567890123456", "bbbbbbbbbbbbbbbb", Dateidecryption);
+		}
+
 
 		TEST_METHOD(Tools_DisplayBitmap)
 		{
