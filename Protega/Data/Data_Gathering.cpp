d10a21e3d85bc42cd846b76c4f34294668924e60
4ND3R50N <l.pickelin@web.de>
2017-11-14 11:34:36 +0100

    Added new class for data gathering functions

---
+++ Protega/Data/Data_Gathering.cpp
@@ -0,0 +1,12 @@
+#include "../stdafx.h"
+#include "Data_Gathering.h"
+
+
+Data_Gathering::Data_Gathering()
+{
+}
+
+
+Data_Gathering::~Data_Gathering()
+{
+}
---
+++ Protega/Data/Data_Gathering.h
@@ -0,0 +1,8 @@
+#pragma once
+class Data_Gathering
+{
+public:
+	Data_Gathering();
+	~Data_Gathering();
+};
+
