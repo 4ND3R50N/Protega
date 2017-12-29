dada113886d67a6e94752d469cd573af6cb19784
4ND3R50N <l.pickelin@web.de>
2017-12-29 19:18:27 +0100

    Added new static vars

--- Protega/Data/Data_Manager.cpp
+++ Protega/Data/Data_Manager.cpp
@@ -6,7 +6,7 @@
 char* Data_Manager::TARGET_ENVIORMENT_OWNER_NAME = "CodeZero";
 char* Data_Manager::TARGET_ENVIORMENT_DATA_URL = "http://62.138.6.50:13011/CabalOnline/";
 char* Data_Manager::TARGET_ENVIORMENT_HEURISTIC_DATA_FILE_NAME = "Heuristic_Data.csv.enc";
-char* Data_Manager::TARGET_ENVIORMENT_VMP_FILE_NAME = "VMP_Addresses.csv.enc";
+char* Data_Manager::TARGET_ENVIORMENT_FTC_FILE_NAME = "Files_To_Check.csv.enc";
 
 //Local data
 char Data_Manager::LOKAL_DATA_NEWLINE_DELIMITER = '#';
@@ -14,12 +14,16 @@ char Data_Manager::LOKAL_DATA_DELIMITER = ';';
 char* Data_Manager::LOKAL_DATA_FOLDER = ".\\protega\\";
 
 //Network data
+const char* Data_Manager::NETWORK_SERVER_IP = "62.138.6.50";
+const char* Data_Manager::NETWORK_SERVER_PORT = "13001";
+const char* Data_Manager::NETWORK_PROTOCOL_DELIMITER = "~";
+const char* Data_Manager::NETWORK_DATA_DELIMITER = ";";
 const char* Data_Manager::DATA_AES_KEY = "1234567890123456";
 const char* Data_Manager::DATA_AES_IV = "bbbbbbbbbbbbbbbb";
 
 //Content data
 std::string** Data_Manager::sHeuristicTable;
-std::string** Data_Manager::sVMPTable;
+std::string** Data_Manager::sFTCTable;
 #pragma endregion
 
 
@@ -30,7 +34,7 @@ bool Data_Manager::CollectDynamicProtesData()
 	//Download data from web
 	std::stringstream ssUrlCombiner;
 	std::stringstream ssHeuristicFilePathCombiner;
-	std::stringstream ssVMPFilePathCombiner;
+	std::stringstream ssFTCFilePathCombiner;
 
 	ssUrlCombiner << TARGET_ENVIORMENT_DATA_URL << TARGET_ENVIORMENT_HEURISTIC_DATA_FILE_NAME;
 	ssHeuristicFilePathCombiner << LOKAL_DATA_FOLDER << TARGET_ENVIORMENT_HEURISTIC_DATA_FILE_NAME;
@@ -38,21 +42,21 @@ bool Data_Manager::CollectDynamicProtesData()
 	ssUrlCombiner.str(std::string());
 
 
-	ssUrlCombiner << TARGET_ENVIORMENT_DATA_URL << TARGET_ENVIORMENT_OWNER_NAME << '/' << TARGET_ENVIORMENT_VMP_FILE_NAME;
-	ssVMPFilePathCombiner << LOKAL_DATA_FOLDER << TARGET_ENVIORMENT_VMP_FILE_NAME;
-	Data_Gathering::DownloadWebFile(_strdup(ssUrlCombiner.str().c_str()), _strdup(ssVMPFilePathCombiner.str().c_str()));
+	ssUrlCombiner << TARGET_ENVIORMENT_DATA_URL << TARGET_ENVIORMENT_OWNER_NAME << '/' << TARGET_ENVIORMENT_FTC_FILE_NAME;
+	ssFTCFilePathCombiner << LOKAL_DATA_FOLDER << TARGET_ENVIORMENT_FTC_FILE_NAME;
+	Data_Gathering::DownloadWebFile(_strdup(ssUrlCombiner.str().c_str()), _strdup(ssFTCFilePathCombiner.str().c_str()));
 	ssUrlCombiner.str(std::string());
 
 	//Read encrypted string from files
 	std::ifstream isHeuristicFileReader(ssHeuristicFilePathCombiner.str());
 	std::string sTempEncryptedHeuristicData((std::istreambuf_iterator<char>(isHeuristicFileReader)), std::istreambuf_iterator<char>());
 	isHeuristicFileReader.close();
-	std::ifstream isVMPFileReader(ssVMPFilePathCombiner.str());
+	std::ifstream isVMPFileReader(ssFTCFilePathCombiner.str());
 	std::string sTempEncryptedVirtualMemoryData((std::istreambuf_iterator<char>(isVMPFileReader)), std::istreambuf_iterator<char>());
 
 	//Delete files
 	remove(ssHeuristicFilePathCombiner.str().c_str());
-	remove(ssVMPFilePathCombiner.str().c_str());
+	remove(ssFTCFilePathCombiner.str().c_str());
 
 	//Decrypt data
 	std::string sTempDecryptedHeuristicData = CryptoPP_AES_Converter::Decrypt(DATA_AES_KEY, DATA_AES_IV, sTempEncryptedHeuristicData);
@@ -60,7 +64,7 @@ bool Data_Manager::CollectDynamicProtesData()
 
 	//Convert data to lists
 	sHeuristicTable = ConvertStringToMatrix(sTempDecryptedHeuristicData);
-	sVMPTable = ConvertStringToMatrix(sTempDecryptedVirtualMemoryData);
+	sFTCTable = ConvertStringToMatrix(sTempDecryptedVirtualMemoryData);
 	return true;
 }
 
@@ -117,6 +121,36 @@ std::string Data_Manager::GetTargetEnviormentDataUrl()
 std::string Data_Manager::GetTargetEnviormentHeuristicDataFileName()
 {
 	return TARGET_ENVIORMENT_HEURISTIC_DATA_FILE_NAME;
+}
+
+const char * Data_Manager::GetNetworkServerIP()
+{
+	return NETWORK_SERVER_IP;
+}
+
+const char * Data_Manager::GetNetworkServerPort()
+{
+	return NETWORK_SERVER_PORT;
+}
+
+const char * Data_Manager::GetProtocolDelimiter()
+{
+	return NETWORK_PROTOCOL_DELIMITER;
+}
+
+const char * Data_Manager::GetDataDelimiter()
+{
+	return NETWORK_DATA_DELIMITER;
+}
+
+const char * Data_Manager::GetNetworkAesKey()
+{
+	return DATA_AES_KEY;
+}
+
+const char * Data_Manager::GetNetworkAesIV()
+{
+	return DATA_AES_IV;
 }
 
 
--- Protega/Data/Data_Manager.h
+++ Protega/Data/Data_Manager.h
@@ -14,19 +14,23 @@ private:
 	static char* TARGET_ENVIORMENT_OWNER_NAME;
 	static char* TARGET_ENVIORMENT_DATA_URL;
 	static char* TARGET_ENVIORMENT_HEURISTIC_DATA_FILE_NAME;
-	static char* TARGET_ENVIORMENT_VMP_FILE_NAME;
+	static char* TARGET_ENVIORMENT_FTC_FILE_NAME;
 
 	static char* LOKAL_DATA_FOLDER;
 	static char LOKAL_DATA_NEWLINE_DELIMITER;
 	static char LOKAL_DATA_DELIMITER;
 
-
+	static const char* NETWORK_SERVER_IP;
+	static const char* NETWORK_SERVER_PORT;
+	static const char* NETWORK_PROTOCOL_DELIMITER;
+	static const char* NETWORK_DATA_DELIMITER;
+	//INFO: Currently we have only 1 pair of AES values. We need 2 for Data and network later!
 	static const char* DATA_AES_KEY;
 	static const char* DATA_AES_IV;
 	
 	//Dynamic Data Storage Vars
 	static std::string** sHeuristicTable;
-	static std::string** sVMPTable;
+	static std::string** sFTCTable;
 
 	//Functions
 	static std::string** ConvertStringToMatrix(std::string sData);
@@ -38,7 +42,12 @@ public:
 	//Getter
 	static std::string GetTargetEnviormentDataUrl();
 	static std::string GetTargetEnviormentHeuristicDataFileName();
-	
+	static const char* GetNetworkServerIP();
+	static const char* GetNetworkServerPort();
+	static const char* GetProtocolDelimiter();
+	static const char* GetDataDelimiter();
+	static const char* GetNetworkAesKey();
+	static const char* GetNetworkAesIV();
 		
 
 
