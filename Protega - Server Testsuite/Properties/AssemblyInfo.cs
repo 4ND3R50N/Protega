a9513e8b72bed17366b999bbe511feedc649fb83
Ferity145 <32592869+Ferity145@users.noreply.github.com>
2018-01-17 20:14:12 +0100

    Server - Test Suite created

---
+++ Protega - Server Testsuite/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Protega - Server Testsuite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Vaillant Group")]
+[assembly: AssemblyProduct("Protega - Server Testsuite")]
+[assembly: AssemblyCopyright("Copyright © Vaillant Group 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("52ce1ee9-a9a1-43f1-9fa8-fd475563cc96")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
