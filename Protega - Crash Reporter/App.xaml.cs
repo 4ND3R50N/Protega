07786e64927d677c5712dec4ad8de224769d3d01
4ND3R50N <l.pickelin@web.de>
2018-02-01 00:01:51 +0100

    Added new project "crash reporter"

---
+++ Protega - Crash Reporter/App.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+namespace Protega___Crash_Reporter
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+
+            //REFINE!!!!
+            StreamReader file = null;
+            string sFilePath = AppDomain.CurrentDomain.BaseDirectory + "latest_protega_error.err";
+            string sError = "";
+                   
+            try
+            {
+                using (FileStream stream = File.Open(sFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            sError = sError + reader.ReadLine();
+                        }
+                    }
+                }
+
+            }
+            catch (Exception de)
+            {
+                string m = de.Message;
+                sError = "The CrashReporter was not able to load the error file!";
+                StartErrorWindow(sError);
+                return;
+            }
+      
+            File.Delete(sFilePath);
+            StartErrorWindow(sError);
+          
+            
+        }
+
+        void StartErrorWindow(string sError)
+        {
+            MainWindow mw = new MainWindow(sError);
+            mw.Show();
+            mw.Activate();
+        }
+
+    }
+}
---
+++ Protega - Crash Reporter/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Protega___Crash_Reporter
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(string sMessage)
+        {
+            InitializeComponent();
+            MainWindow1.Activate();
+            rtbError.AppendText(sMessage);
+            MainWindow1.Activate();
+
+        }
+        
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
